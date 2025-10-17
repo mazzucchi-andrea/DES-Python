36241e5c73b14689f2a6eb402949189986fde672
mazzucchi-andrea <mazzucchiandrea@gmail.com>
2025-10-17 08:15:57 +0200

    code update and clean up

--- acs.py
+++ acs.py
@@ -1,94 +1,90 @@
-# # -------------------------------------------------------------------------  
-#  * This program is based on a one-pass algorithm for the calculation of an  
-#  * array of autocorrelations r[1], r[2], ... r[K].  The key feature of this 
-#  * algorithm is the circular array 'hold' which stores the (K + 1) most 
-#  * recent data points and the associated index 'p' which points to the 
-#  * (rotating) head of the array. 
-#  * 
-#  * Data is read from a text file in the format 1-data-point-per-line (with 
-#  * no blank lines).  Similar to programs UVS and BVS, this program is
-#  * designed to be used with OS redirection. 
-#  * 
-#  * NOTE: the constant K (maximum lag) MUST be smaller than the # of data 
-#  * points in the text file, n.  Moreover, if the autocorrelations are to be 
-#  * statistically meaningful, K should be MUCH smaller than n. 
-#  *
-#  * Name              : acs.c  (AutoCorrelation Statistics) 
-#  * Author            : Steve Park & Dave Geyer 
-#  * Language          : ANSI C
-#  * Latest Revision   : 2-10-97 
-#  * Compile with      : gcc -lm acs.c
-#  * Execute with      : acs.out < acs.dat
-#  # Translated by     : Philip Steele 
-#  # Language          : Python 3.3
-#  # Latest Revision   : 3/26/14
-#  * Execute with      : python acs.py < acs.dat
-#  * ------------------------------------------------------------------------- 
-#  */
+"""
+This program is based on a one-pass algorithm for the calculation of an array of autocorrelations r[1], r[2], ... r[K].
+The key feature of this algorithm is the circular array 'hold' which stores the (K + 1) most recent data points and the
+associated index 'p' which points to the (rotating) head of the array.
 
-#include <stdio.h>
-#include <math.h>
+Data is read from a text file in the format 1-data-point-per-line (with no blank lines).
+
+NOTE: the constant K (maximum lag) MUST be smaller than the of data points in the text file, n. Moreover, if the
+autocorrelations are to be statistically meaningful, K should be MUCH smaller than n.
+
+Name            : acs.c  (AutoCorrelation Statistics)
+Author          : Steve Park & Dave Geyer
+Language        : ANSI C
+Latest Revision : 10/02/1997
+Compile with    : gcc -lm acs.c
+Execute with    : acs.out < acs.dat
+Translated by   : Philip Steele
+Language        : Python 3.3
+Latest Revision : 26/03/2014
+Execute with    : python acs.py < acs.dat
+Updated by      : Andrea Mazzucchi
+Python Version  : 3.17
+Latest Revision : 17/10/2025
+Execute with    : python acs.py acs.dat
+"""
 
 import sys
-from math import sqrt
 
-K = 50                             # K is the maximum lag */
+import numpy as np
+
+K = 50  # K is the maximum lag
 SIZE = (K + 1)
 
+_sum = 0.0  # sums x[i]
+hold = []  # K + 1 most recent data points
+p = 0  # points to the head of 'hold'
+co_sum = [0 for _ in range(0, SIZE)]  # co_sum[j] sums x[i]  x[i+j]
 
-i = 0                          # data point index              */
-sum = 0.0                      # sums x[i]                     */
-j = 0                          # lag index                     */
-hold = []                      # K + 1 most recent data points */
-p = 0                          # points to the head of 'hold'  */
-cosum = [0 for i in range(0,SIZE)]    # cosum[j] sums x[i] * x[i+j]   */
+file_path = None
+if len(sys.argv) > 1:
+    file_path = sys.argv[1]
+else:
+    print("Error: Please provide a file path as the first argument.")
 
+i = 0  # data point index
+with open(file_path, 'r') as file:
+    for line in file:
+        x = float(line)
+        _sum += x
+        hold.append(x)
+        i += 1
+        if i == SIZE:
+            break
 
-while (i < SIZE):              # initialize the hold array with */
-  x = float(sys.stdin.readline())     # the first K + 1 data values    */
-  sum += x
-  hold.append(x)
-  i += 1
-#EndWhile
+    for line in file:
+        for j in range(0, SIZE):
+            co_sum[j] += hold[p] * hold[(p + j) % SIZE]
+        x = float(line)  # lines read in as string
+        _sum += x
+        hold[p] = x
+        p = (p + 1) % SIZE
+        i += 1
 
-x = sys.stdin.readline()   
+n = i  # the total number of data points
 
-while (x):
-  for j in range(0,SIZE):
-    cosum[j] += hold[p] * hold[(p + j) % SIZE]
-  x = float(x) #lines read in as string
-  sum    += x
-  hold[p] = x
-  p       = (p + 1) % SIZE
-  i += 1 
-  x = sys.stdin.readline()
-#EndWhile
-n = i #the total number of data points
+while i < n + SIZE:  # empty the circular array
+    for j in range(0, SIZE):
+        co_sum[j] += hold[p] * hold[(p + j) % SIZE]
+    hold[p] = 0.0
+    p = (p + 1) % SIZE
+    i += 1
 
-while (i < n + SIZE):         # empty the circular array */
-  for j in range(0,SIZE):
-    cosum[j] += hold[p] * hold[(p + j) % SIZE]
-  hold[p] = 0.0
-  p       = (p + 1) % SIZE
-  i += 1 
-#EndWhile
-
-mean = sum / n
-for j in range(0,K+1):  
-  cosum[j] = (cosum[j] / (n - j)) - (mean * mean)
+mean = _sum / n
+for j in range(0, K + 1):
+    co_sum[j] = (co_sum[j] / (n - j)) - (mean * mean)
 
 print("for {0} data points".format(n))
-print("the mean is ... {0:8.2f}".format(mean))
-print("the stdev is .. {0:8.2f}\n".format(sqrt(cosum[0])))
+print("the mean is ... \t{0:8.2f}".format(mean))
+print("the std_dev is ... \t{0:8.2f}\n".format(np.sqrt(co_sum[0])))
 print("  j (lag)   r[j] (autocorrelation)\n")
-for j in range(1,SIZE):
-  print("{0:3d}  {1:11.3f}".format(j, cosum[j] / cosum[0]))
+for j in range(1, SIZE):
+    print("{0:3d}  {1:11.3f}".format(j, co_sum[j] / co_sum[0]))
 
-
-# C output:
+# Output with acs.dat:
 # for 500 data points
-# the mean is ...     5.68
-# the stdev is ..     4.72
+# the mean is ...       5.68
+# the std_dev is ...    4.72
 
 #   j (lag)   r[j] (autocorrelation)
 #   1        0.966
